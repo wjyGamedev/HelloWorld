8a1eec69b01db4f6c96eb06f505098ab643b77c9
wjyGamedev <wjyGamedev@126.com>
2014-06-14 12:46:31 +0800

    HelloWorld
    code

---
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,15 @@
+// HelloWorld.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+using std::cout;
+using std::endl;
+using std::cin;
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout<<"test"<<endl;
+	system("pause");
+	return 0;
+}
+
---
+++ HelloWorld/HelloWorld/HelloWorld/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,17 @@
+// HelloWorld.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout<<"test"<<endl;
+	system("pause");
+	return 0;
+}
+
